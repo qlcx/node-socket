157e860765e47b3d281103d7672cb900407168e4
qlcx <qlcx@qlcxdeMacBook-Pro.local>
2017-03-07 07:36:02 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const { resolve } = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './index.js'
+  ],
+    context: resolve(__dirname, 'app'),
+
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+    contentBase: resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?modules',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+}
