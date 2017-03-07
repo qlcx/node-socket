903ccc18c85c757564c3508046ff1642bc6cbe16
qlcx <1136425019@qq.com>
2017-03-07 09:46:13 +0800

    add koa

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const Koa = require('koa')
+const logger = require('koa-logger')
+const koaStatic = require('koa-static')
+const views = require('koa-views')
+const path = require('path')
+
+const webpack = require('webpack')
+const webpackConf = require('../webpack.config')
+
+
+const app = new Koa()
+
+// const WebpackDevServer = require('webpack-dev-server')
+// if (process.env.NODE_ENV != 'prod') {
+//     let webpackDevServer = new WebpackDevServer(webpack(webpackConf), webpackConf.devServer)
+//     webpackDevServer.listen(8080, "localhost", () => {
+//         console.log('Listening on port 8080!')
+//     })
+// } else {
+//     // webpack-dev-server serves all static assets from memory in development mode
+//     app.use(koaStatic(path.join(__dirname, './static/dist/')))    
+// }
+
+app.use(logger())
+app.use(koaStatic(path.join(__dirname, '../dist/')))    
+app.use(views(path.join(__dirname, '../dist/views')))
+app.use(async(ctx) => {
+    await ctx.render('index')
+})
+
+// const server = require('http').createServer(app.callback())
+// const io = require('socket.io')(server)
+// io.on('connection', () => {
+//     console.log('connect')
+// })
+
+app.listen(7000)
